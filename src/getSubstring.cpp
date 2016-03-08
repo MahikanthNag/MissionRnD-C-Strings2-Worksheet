51fd6c57ef34ffa06c06fe613b72f0b1c09b9c7c
MahikanthNag <MahikanthNag@missionrnd.com>
2016-03-08 22:03:55 +0530

    Submit Commit

--- src/getSubstring.cpp
+++ src/getSubstring.cpp
@@ -13,11 +13,43 @@ OUTPUT2:will Code
 NOTES: Check Null Strings ,Include stddef for NULL if needed.Create a New string and return it ,dont modfiy
 original String
 */
-
+#include<stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
-
+#include<malloc.h>
 char * get_sub_string(char *str, int i, int j){
 
-    return NULL;
+    int x=i;int z=0;
+    char *res;
+    res=(char*)malloc(100);
+    if(str==NULL)
+        return NULL;
+    int len=0;
+    for(len=0;str[len]!='\0';len++){}
+    if(i>j)
+            return NULL;
+    else
+    {
+
+        while(x<=j)
+        {
+            res[z]=str[x];
+            z++;
+            x++;
+        }
+        return res;
+
+    }
+
+
 }
+/*int main()
+{
+    char *str;
+    str=(char*)malloc(100);
+    scanf("%s",str);
+    printf("%s is the answer",get_sub_string(str,0,2));
+    return 0;
+
+}
+*/
